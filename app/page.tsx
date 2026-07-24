e7357b17076db31bd55d310d1a806de82b39b922
Vancrown <lanxirye@outlook.com>
2026-07-24 16:51:27 -0400

    fix footer rss path, update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">eii...</h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hi, this is eii...
+        `}
       </p>
+      <p className="mb-0">{`Welcome to my landing page.`}</p>
+      <p className="mb-0">{`I am sharing my boring journels, views and poems here.`}</p>
+      <p className="mb-0">{`I know I am writing nonsense.`}</p>
+      <p className="mb-4">{`If you don't like them, please leave the page quitely and do not let me know.
+       `}</p>
+      <p className="mb-4">{`Wish you had a great time here. `}</p>
+
+      <div className="py-4">{}</div>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+const repoName = "landing";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+
+  basePath: isProduction ? `/${repoName}` : "",
+
   images: {
     unoptimized: true,
   },
